3b52bc7923ed4220d2d64dac8154fe8ac9ceca5d
unknown <idrice.tsafouet@yahoo.com>
2026-07-08 04:59:46 +0200

    create account dashboard #53 #52

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .models import Account
 
 # Create your views here.
+@login_required
+def account_list(request):
+    accounts = Account.objects.filter(
+        user=request.user,
+        is_active=True
+    )
+
+    context = {
+        "accounts": accounts,
+    }
+
+    return render(
+        request,
+        "accounts/account_list.html", context
+    )
