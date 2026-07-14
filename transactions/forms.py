40e69c01a1337f5be987c36852468ce0e1d7c4e4
unknown <idrice.tsafouet@yahoo.com>
2026-07-14 03:47:10 +0200

    Create forms.py #65

---
+++ transactions/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Transaction
+from categories.models import Category
+from accounts.models import Account
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = [
+            "account",
+            "category",
+            "amount",
+            "transaction_date",
+            "description",
+            "notes"
+        ]
+
+        widgets = {
+            "transaction_date": forms.DateInput(
+                attrs={"type": "date"}
+            ),
+            "notes": forms.Textarea(
+                attrs={"rows": 4}
+            ),
+        }
+    
+    def __init__(self, *args, user=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if user:
+            self.fields["account"].queryset = Account.objects.filter(
+                user=user,
+                is_active=True
+            )
+            self.fields["category"].queryset = Category.objects.filter(
+                user=user,
+                is_active=True
+            )
